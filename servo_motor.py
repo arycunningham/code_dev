4b8f7e4222abdbe77036577a0598130b416be6dc
brayk1908 <brayk1908@gmail.com>
2025-09-04 15:12:23 +1000

    Added servo B

--- servo_motor.py
+++ servo_motor.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+#needs to run 
+	#sudo pigpiod
+	#export PORT_ADDR=soft
+	#export PORT_ADDR=soft
+		#rostopic pub /actuator_control/actuator_a std_msgs/Bool '{data: True}'
 import rospy
 import RPi.GPIO as GPIO
 from std_msgs.msg import Bool
@@ -13,23 +18,36 @@ from time import sleep
 
 factory = PiGPIOFactory()
 sub_a = None
-servo = AngularServo(12, min_angle=-90, max_angle=90,pin_factory=factory)
+servo_a = AngularServo(12, min_angle=-90, max_angle=90,pin_factory=factory)
+servo_b = AngularServo(12, min_angle=-90, max_angle=90,pin_factory=factory)
 
 def callback_a(msg_in):
 	# A bool message contains one field called "data" which can be true or false
-	#data set to true
-	#rostopic pub /actuator_control/actuator_a std_msgs/Bool '{data: True}'
 	# http://docs.ros.org/melodic/api/std_msgs/html/msg/Bool.html
-	# XXX: The following "GPIO.output" should be replaced to meet the needs of your actuators!
 	if msg_in.data:
-		rospy.loginfo("Setting output high!")
-		servo.angle=90
+		rospy.loginfo("Rotating Servo A")
+		servo_a.angle=90
 		sleep(1)
-		servo.angle=-90
+		servo_a.angle=-90
 		sleep(1)
 	else:
-		rospy.loginfo("Setting output low!")
-		servo.angle=-90
+		rospy.loginfo("Center Point")
+		servo_a.angle=-90
+		sleep(1)
+
+sub_b = None
+servo_b = AngularServo(12, min_angle=-90, max_angle=90,pin_factory=factory)
+
+def callback_b(msg_in):
+	if msg_in.data:
+		rospy.loginfo("Rotating Servo B")
+		servo_b.angle=90
+		sleep(1)
+		servo_b.angle=-90
+		sleep(1)
+	else:
+		rospy.loginfo("Center Point")
+		servo_b.angle=-90
 		sleep(1)
 
 
@@ -37,6 +55,8 @@ def shutdown():
 	# Clean up our ROS subscriber if they were set, avoids error messages in logs
 	if sub_a is not None:
 		sub_a.unregister()
+	if sub_b is not None:
+		sub_b.unregister()
 
 	# XXX: Could perform some failsafe actions here!
 
@@ -47,6 +67,7 @@ if __name__ == '__main__':
 
 	# Setup the publisher for a single actuator (use additional subscribers for extra actuators)
 	sub_a = rospy.Subscriber('/actuator_control/actuator_a', Bool, callback_a)
+	sub_b = rospy.Subscriber('/actuator_control/actuator_b', Bool, callback_b
 
 	# Make sure we clean up all our code before exiting
 	rospy.on_shutdown(shutdown)
