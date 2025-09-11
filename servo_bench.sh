d792c46ca998b7c6e38eb63a2380168a99918799
arycunningham <ary.cunningham1@gmail.com>
2025-09-12 01:24:46 +1000

    fine-tuning shell script for servo benchmarking

--- servo_bench.sh
+++ servo_bench.sh
@@ -1,7 +1,11 @@
 rostopic pub /actuator_control/actuator_a std_msgs/Bool '{data: True}'
 sleep 2
+exit 0
 rostopic pub /actuator_control/actuator_a std_msgs/Bool '{data: False}'
 sleep 2
+exit 0
 rostopic pub /actuator_control/actuator_b std_msgs/Bool '{data: True}'
 sleep 2
+exit 0
 rostopic pub /actuator_control/actuator_a std_msgs/Bool '{data: False}'
+exit 0
